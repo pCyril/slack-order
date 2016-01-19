132426e55a035e6934e07d3259ec0d27b5967fe3
cyril <perraud.cyril@gmail.com>
2016-01-19 20:39:30 +0100

    Initial commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Service\BonjourMadameService;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @template
+     */
+    public function indexAction(Request $request)
+    {
+        return [];
+    }
+
+    /**
+     * @Route("/excuses", name="excuse")
+     */
+    public function excuseAction(Request $request)
+    {
+
+    }
+
+    /**
+     * @Route("/bonjour", name="bonjour")
+     */
+    public function lastBonjourMadameAction(Request $request)
+    {
+        if ($request->get('token') !== 'wReTCvIBqSJoD3mKzXWWWHsx') {
+            throw new \InvalidArgumentException('Bad token');
+        }
+        /** @var BonjourMadameService $bonjourMadameService */
+        $bonjourMadameService = $this->get('bonjour_madame_sevice');
+        $image = $bonjourMadameService->getLastBonjourMadameImage();
+
+        return new JsonResponse([
+            'text' => 'Vicieux !!!',
+            'attachments' => [
+                'image_url' => $image,
+            ],
+        ]);
+    }
+}
