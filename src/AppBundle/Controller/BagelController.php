f672a11547814b91b3491591f84871f77d7a8fdd
cyril <perraud.cyril@gmail.com>
2016-01-20 23:23:48 +0100

    BagelCommandService in progress+++

--- src/AppBundle/Controller/BagelController.php
+++ src/AppBundle/Controller/BagelController.php
@@ -22,12 +22,13 @@ class BagelController extends Controller
         /** @var BagelCommandService $bagelCommandService */
         $bagelCommandService = $this->get('bonjour_madame_sevice');
 
-        $text = $request->get('text') ? $request->get('text') : BagelCommandService::BAGEL_COMMAND_HELP;
+        $text = !empty($request->get('text')) ? $request->get('text') : BagelCommandService::BAGEL_COMMAND_HELP;
 
         $textExploded = explode(' ', $text);
 
         switch($textExploded[0]) {
             case BagelCommandService::BAGEL_COMMAND_HELP:
+            default:
                 $data = $bagelCommandService->help();
                 break;
             case BagelCommandService::BAGEL_COMMAND_CANCEL:
@@ -37,11 +38,12 @@ class BagelController extends Controller
                 $data = $bagelCommandService->orderList();
                 break;
             case BagelCommandService::BAGEL_COMMAND_ORDER:
-                $data = $bagelCommandService->addOrder($request->get('user_name'), $textExploded[1]);
-                break;
-            default:
+                unset($textExploded[0]);
+                $data = $bagelCommandService->addOrder($request->get('user_name'), implode(" ", $textExploded));
                 break;
         }
+
+        return new JsonResponse($data);
     }
 
 }
--- src/AppBundle/Service/BagelCommandService.php
+++ src/AppBundle/Service/BagelCommandService.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Service;
 
 
+use AppBundle\Entity\Bagel;
 use AppBundle\Repository\BagelRepository;
 use Doctrine\ORM\EntityManager;
 
@@ -34,7 +35,53 @@ class BagelCommandService {
         /** @var BagelRepository $bagelRepository */
         $bagelRepository = $this->em->getRepository('AppBundle:Bagel');
 
-        return [];
+        $currentDate = new \DateTime();
+        $startDate = new \DateTime();
+        $startDate->setTime(8, 0, 0);
+        $endDate = new \DateTime();
+        $endDate->setTime(11, 10, 0);
+        if ($currentDate >= $startDate && $currentDate <= $endDate) {
+            $currentDate->setTime(0, 0, 0);
+            $bagel = $bagelRepository->findOneBy(['name' => $name, 'date' => $currentDate]);
+
+            $bagel = $bagel ? $bagel : new Bagel();
+
+            $bagel
+                ->setName($name)
+                ->setDate($currentDate)
+                ->setOrder($order);
+
+            $this->em->persist($bagel);
+            $this->em->flush();
+
+            return [
+                'response_type' => 'in_channel',
+                'text' => sprintf('%s a commandé un bagel pour ce midi.', $name),
+                'attachments' => [
+                    [
+                        'fallback' => 'Fail ?',
+                        'text' => sprintf('Il devrait manger à midi un `%s`, bon appétit!', $order),
+                        'mrkdwn' => true,
+                    ],
+                    [
+                        'fallback' => 'Fail ?',
+                        'text' => 'Tu souhaites toi aussi manger un bagel ? Utilise la commande `/bagel`',
+                        'mrkdwn' => true,
+                    ],
+                ],
+            ];
+        } else {
+
+            return [
+                'text' => 'Désolé les commandes ne sont accéptés que de 8:00 à 11:10',
+                'attachments' => [
+                    [
+                        'fallback' => 'Fail ?',
+                        'text' => 'Tu peux quand même appeler Bagel Time au 04 78 43 52 19',
+                    ],
+                ],
+            ];
+        }
     }
 
     /**
@@ -46,7 +93,37 @@ class BagelCommandService {
         /** @var BagelRepository $bagelRepository */
         $bagelRepository = $this->em->getRepository('AppBundle:Bagel');
 
-        return [];
+        $currentDate = new \DateTime();
+        $startDate = new \DateTime();
+        $startDate->setTime(8, 0, 0);
+        $endDate = new \DateTime();
+        $endDate->setTime(11, 10, 0);
+        if ($currentDate >= $startDate && $currentDate <= $endDate) {
+            $currentDate->setTime(0, 0, 0);
+            $bagel = $bagelRepository->findOneBy(['name' => $name, 'date' => $currentDate]);
+            if (null === $bagel) {
+                return [
+                    'text' => 'Tu n\avais rien commandé, mais dans le doute tu as bien fait.',
+                ];
+            }
+
+            $this->em->remove($bagel);
+            $this->em->flush();
+
+            return [
+                'text' => 'Ta commande a bien été annulée.',
+            ];
+        } else {
+            return [
+                'text' => 'Il est trop tard pour annuler ta commande.',
+                'attachments' => [
+                    [
+                        'fallback' => 'Fail ?',
+                        'text' => 'Tu peux quand même essayer d\'appeler Bagel Time au 04 78 43 52 19',
+                    ],
+                ],
+            ];
+        }
     }
 
     /**
@@ -57,6 +134,10 @@ class BagelCommandService {
         /** @var BagelRepository $bagelRepository */
         $bagelRepository = $this->em->getRepository('AppBundle:Bagel');
 
+        $currentDate = new \DateTime();
+        $currentDate->setTime(0, 0, 0);
+        $bagels = $bagelRepository->findBy(['date' => $currentDate]);
+
         return [];
     }
 
@@ -65,6 +146,30 @@ class BagelCommandService {
      */
     public function help()
     {
-        return [];
+        return [
+            'text' => 'Tu as faim mais tu ne sais pas comment faire ?',
+            'attachments' => [
+                [
+                    'fallback' => 'Fail ?',
+                    'text' => 'Si tu souhaites passer ou modifier une commande.\n `/bagel commande Grenoblois/Pavot/Tartare`',
+                    'mrkdwn' => true,
+                ],
+                [
+                    'fallback' => 'Fail ?',
+                    'text' => 'Si tu n\'as plus faim.\n `/bagel annuler`',
+                    'mrkdwn' => true,
+                ],
+                [
+                    'fallback' => 'Fail ?',
+                    'text' => 'Tu souhaites savoir avec qui tu vas manger ?.\n `/bagel liste`',
+                    'mrkdwn' => true,
+                ],
+                [
+                    'fallback' => 'Fail ?',
+                    'text' => 'Important: Tu as jusqu\'à 11h10 pour passer ta commande.',
+                    'color' => 'danger',
+                ],
+            ],
+        ];
     }
 }
